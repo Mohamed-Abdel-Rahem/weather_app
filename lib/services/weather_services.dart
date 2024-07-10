92abd3a88edade7adae7b3147de390612d5d53a3
Mohamed-Abdel-Rahem <mohammedabdelrahem202123067@gmail.com>
2024-07-10 18:36:01 +0300

    add handling
    add try and catch to handle exception

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -8,10 +8,14 @@ class WeatherServices {
   // 'https://api.weatherapi.com/v1/forecast.json?
   // key=9f2c3c3e31e0499982804829241007&q=$cityName&days=1'
   WeatherServices({required this.dio});
-  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response =
-        await dio.get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
-    WeatherModel weatherModel = WeatherModel.fromJason(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+      WeatherModel weatherModel = WeatherModel.fromJason(response.data);
+      return weatherModel;
+    } catch (e) {
+      return null;
+    }
   }
 }
